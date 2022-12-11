4fa66888688ead74afce90db5418eb1fa28d32f0
biosnod <biosnod@gmail.com>
2022-12-11 12:37:52 +0300

    Move versions to the separate script

---
+++ versions.release.js
@@ -0,0 +1,158 @@
+const list =
+    {
+        '1.0_live': [
+            {
+                res: {Version: 1284249, Suffix: "ba7ad33643"},
+                clientSilence: {Version: 1393824, Suffix: "52338ab3d4"},
+                client: {Version: 1358691, Suffix: "52338ab3d4"},
+            },
+        ],
+
+        '1.4_live': [
+            {
+                res: {Version: 2381708, Suffix: "53b1e84ce4"},
+                clientSilence: {Version: 2396714, Suffix: "52338ab3d4"},
+                client: {Version: 2396714, Suffix: "52338ab3d4"},
+            },
+        ],
+
+        '1.5_live': [
+            {
+                res: {Version: 2484655, Suffix: "d25bf77fa4"},
+                clientSilence: {Version: 2494513, Suffix: "3ddaee14c9"},
+                client: {Version: 2502139, Suffix: "d0d890eb69"},
+            },
+
+            {
+                res: {Version: 2663089, Suffix: "25a12eeea5"},
+                clientSilence: {Version: 2771742, Suffix: "99f775138c"},
+                client: {Version: 2762856, Suffix: "4ebb02e19b"},
+            },
+
+            {
+                res: {Version: 2775769, Suffix: "c196739952"},
+                clientSilence: {Version: 2785677, Suffix: "dd8d2f28b5"},
+                client: {Version: 2780670, Suffix: "19133fa79e"},
+            },
+
+            {
+                clientSilence: {Version: 2797188, Suffix: "f772fde953"},
+                client: {Version: 2808729, Suffix: "7a9522338e"},
+            },
+
+            {
+                res: {Version: 2866676, Suffix: "eaf0bb745e"},
+                clientSilence: {Version: 2901119, Suffix: "aae6a5b3eb"},
+                client: {Version: 2934686, Suffix: "907ecc1dcb"},
+            },
+        ],
+
+        '1.6_live': [
+            {
+                res: {Version: 3305047, Suffix: "ecff173daf"},
+                clientSilence: {Version: 3266913, Suffix: "b1501b6955"},
+                client: {Version: 3353303, Suffix: "242157b1de"},
+            },
+
+            {
+                res: {Version: 3557509, Suffix: "5979a935f8"},
+                client: {Version: 3526661, Suffix: "2b3db51945"},
+            },
+        ],
+
+        '2.7_live': [
+            {
+                res: {Version: 6855943, Suffix: "1490a59df9"},
+                clientSilence: {Version: 6801534, Suffix: "69e473b331"},
+                client: {Version: 6902895, Suffix: "d6252eb166"},
+            }
+        ],
+
+        '3.0_live': [
+            {
+                // base version with sound data in AudioAssets (Japanese, Korean, etc...)
+                res: {Version: 9624836, Suffix: "ed0599bc5b"},
+            },
+
+            {
+                res: {Version: 10283122, Suffix: "ec58ff372e"},
+                clientSilence: {Version: 10446836, Suffix: "320895326e"},
+                client: {Version: 10316937, Suffix: "4fcac11e23"},
+            }
+        ],
+
+        '3.1_live': [
+            {
+                res: {Version: 10345388, Suffix: "db455059e3"},
+                clientSilence: {Version: 10289512, Suffix: "36a8dc6d48"},
+                client: {Version: 10350544, Suffix: "7a97cd5fbb"},
+            },
+
+            {
+                // base version with sound data in AudioAssets (Japanese, Korean, etc...)
+                res: {Version: 10457664, Suffix: "3bdde23eb8"},
+            },
+
+            {
+                res: {Version: 10676272, Suffix: "ab446cd9d8"},
+                clientSilence: {Version: 10660688, Suffix: "b69559f811"},
+                client: {Version: 10693398, Suffix: "99e1a99fd2"},
+            },
+
+            {
+                clientSilence: {Version: 10660688, Suffix: "b69559f811"},
+                client: {Version: 10772333, Suffix: "09136b2529"},
+            },
+
+            {
+                clientSilence: {Version: 10805493, Suffix: "3222597d09"},
+                client: {Version: 10772333, Suffix: "09136b2529"},
+            },
+
+            {
+                res: {Version: 10916590, Suffix: "847ba6bd45"},
+                clientSilence: {Version: 10805493, Suffix: "3222597d09"},
+                client: {Version: 10941477, Suffix: "af48dac880"},
+            }
+        ],
+
+        '3.2_live': [
+            {
+                res: {Version: 11149961, Suffix: "d6b14858bc"},
+                clientSilence: {Version: 11212885, Suffix: "766b0a2560"},
+                client: {Version: 11319614, Suffix: "f9c5287efd"},
+            },
+
+            {
+                res: {Version: 11353770, Suffix: "6428631800"},
+                client: {Version: 11364183, Suffix: "175a3e3bff"},
+            },
+
+            {
+                client: {Version: 11404032, Suffix: "d92901d0b2"},
+            },
+
+            {
+                res: {Version: 11611027, Suffix: "8bf0cf5f3d"},
+                client: {Version: 11793813, Suffix: "1bb7eba264"},
+            },
+        ],
+
+        '3.3_live': [
+            {
+                res: {Version: 11972804, Suffix: "a46af65d51"},
+                clientSilence: {Version: 11938958, Suffix: "b040c0ebeb"},
+                client: {Version: 12136874, Suffix: "f3a4b38d81"},
+            },
+        ],
+
+        '3.4_live': [
+            {
+                res: {Version: 11994380, Suffix: "6091cf4556"},
+                clientSilence: {Version: 12103638, Suffix: "774f2bebb4"},
+                client: {Version: 12117976, Suffix: "9ddf06370b"},
+            },
+        ],
+    }
+
+module.exports = {list}
