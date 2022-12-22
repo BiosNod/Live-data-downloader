129f43cef6460947bf777df30d9c01c5acff7e57
biosnod <biosnod@gmail.com>
2022-12-22 12:21:03 +0300

    Fix waiting for downloading, del control chars

--- helpers.js
+++ helpers.js
@@ -3,6 +3,7 @@ const util = require("util");
 const stream = require("stream");
 const https = require('https');
 const http = require('http');
+const { finished } = require('node:stream/promises');
 
 const unlinkIfExists = (filePath) => fs.existsSync(filePath) ? fs.unlinkSync(filePath) : ''
 // Check and remove zero size files (this can slow down data processing)
@@ -40,17 +41,21 @@ async function forceDownload(link, fileFullPath) {
     // if you want to download only with some ext
     // if (link.indexOf('.zip') > -1) return
 
+    // Remove all control characters in Unicode
+    fileFullPath = fileFullPath.replace(/[\u0000-\u001F\u007F-\u009F]/g, "")
+    // Check zero files
     if (isDelZeroFiles) checkAndDelZeroFile(fileFullPath)
 
     const fileFolder = fileFullPath.split('/').slice(0, -1).join('/')
 
+    console.log('fileFullPath', fileFullPath)
     if (!fs.existsSync(fileFullPath)) {
-        // console.log("creating dir:", fileFolder)
+        console.log(`creating dir:`, fileFolder)
         fs.mkdirSync(fileFolder, {recursive: true})
         try {
-            const tmpExt = '.~tmp'
-            const fileFullTempPath = fileFullPath + tmpExt
+            const fileFullTempPath = fileFullPath + '.~tmp'
 
+            console.log(`Creating temp file`, fileFullTempPath)
             // Maybe previous download process crashed
             unlinkIfExists(fileFullTempPath)
 
@@ -64,10 +69,6 @@ async function forceDownload(link, fileFullPath) {
 
                 request.on('response', (response) => {
                     response.pipe(fileStream)
-
-                    // Rename tmp file name to the original name
-                    fs.renameSync(fileFullTempPath, fileFullPath)
-
                     resolve(response);
                 });
 
@@ -76,6 +77,12 @@ async function forceDownload(link, fileFullPath) {
                     resolve(err);
                 })
             }))
+
+            // Waiting responce pipe
+            await finished(fileStream);
+
+            // Rename tmp file name to the original name
+            fs.renameSync(fileFullTempPath, fileFullPath)
 
             // Unlink tmp file if some error occurs while downloading
             unlinkIfExists(fileFullTempPath)
